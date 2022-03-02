eeead40c025cc580e39811c165090ab6c5433bca
April Hughes <dreydain@gmail.com>
2022-03-02 12:29:56 -0800

    image slide show functionality completed and made code more DRY

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 'use strict';
 
-// 2. Porfolio Functionality:
-//C. Add slide project slide show feature to active projects
-
 // 3. Bonus addition:
 //A. CSS on hover change color of social btns to blue or green
 
@@ -25,28 +22,17 @@ function removeActiveClasses() {
 	});
 }
 
-//Functions for project slide show:
-// slide show should only start if project is active
-// slide show should stop once project is closed
-// slide show should default to cover image when not active
-
+//Portfolio Projects Functionality
 const projects = document.querySelectorAll('.project');
-const projectsArr = Array.from(projects);
+const buttons = document.querySelectorAll('.btn');
 
 const ecommerceImages = document.querySelectorAll('.ecommerce');
 const trainsImages = document.querySelectorAll('.trains');
 const trainingImages = document.querySelectorAll('.training');
 
 let idx = 0;
-const time = 2000;
-
-function displayCover(images) {
-	images.forEach((image) => {
-		image.classList.add('hide');
-	});
-	images[0].classList.remove('hide');
-	clearTimeout(imgSlideShow, 2000);
-}
+const time = 2500;
+setInterval(imgSlideShow, time);
 
 function runSlides(imagesArr) {
 	if (idx === imagesArr.length - 1) {
@@ -57,56 +43,56 @@ function runSlides(imagesArr) {
 
 	imagesArr[idx].classList.add('hide');
 	imagesArr[idx + 1].classList.remove('hide');
-	console.log(imagesArr[idx], imagesArr[idx + 1]);
-
-	console.log(idx);
 }
 
+function displayCover(images) {
+	images.forEach((image) => {
+		image.classList.add('hide');
+	});
+	images[0].classList.remove('hide');
+	clearInterval(imgSlideShow);
+}
+
+// Image Slide Show interval function
 function imgSlideShow() {
 	//if active project is equal to eCommerce
 	if (projects[0].classList.value != 'project') {
-		//run for or while loop function...
 		runSlides(ecommerceImages);
-		setInterval(imgSlideShow, time);
 	} else {
 		displayCover(ecommerceImages);
 	}
-
+	//if active project is equal to model trains
 	if (projects[1].classList.value != 'project') {
-		//run for or while loop function...
 		runSlides(trainsImages);
-		setInterval(imgSlideShow, time);
 	} else {
 		displayCover(trainsImages);
 	}
-
+	//if active project is equal to training project
 	if (projects[2].classList.value != 'project') {
-		//run for or while loop function...
 		runSlides(trainingImages);
-		setInterval(imgSlideShow, time);
 	} else {
 		displayCover(trainingImages);
 	}
 
 	idx++;
-	console.log(projects[1].classList.value);
 }
 
-//Portfolio Functionality
+//Function for removing active project and button classes
+function removeActivePortfolioClasses() {
+	projects.forEach((project) => {
+		project.classList.remove('active');
+	});
 
-//Selecting elements and assigning to an array
-
-const buttons = document.querySelectorAll('.btn');
-const buttonsArr = Array.from(buttons);
-console.log(projects, buttonsArr);
+	buttons.forEach((btn) => {
+		btn.classList.remove('active');
+	});
+}
 
 //Button functionality
-buttonsArr.forEach((button, index) => {
+buttons.forEach((button, index) => {
 	button.addEventListener('click', () => {
 		idx = 0;
-		clearInterval(imgSlideShow);
-		removeActiveProjectClasses();
-		removeActiveBtnClasses();
+		removeActivePortfolioClasses();
 		button.classList.add('active');
 		projects[index].classList.add('active');
 		imgSlideShow();
@@ -114,27 +100,12 @@ buttonsArr.forEach((button, index) => {
 });
 
 //Project Functionality
-projectsArr.forEach((project, index) => {
+projects.forEach((project, index) => {
 	project.addEventListener('click', () => {
 		idx = 0;
-		clearInterval(imgSlideShow);
-		removeActiveProjectClasses();
-		removeActiveBtnClasses();
+		removeActivePortfolioClasses();
 		project.classList.add('active');
 		buttons[index].classList.add('active');
 		imgSlideShow();
 	});
 });
-
-//Functions for removing active classes
-function removeActiveProjectClasses() {
-	projects.forEach((project) => {
-		project.classList.remove('active');
-	});
-}
-
-function removeActiveBtnClasses() {
-	buttons.forEach((btn) => {
-		btn.classList.remove('active');
-	});
-}
